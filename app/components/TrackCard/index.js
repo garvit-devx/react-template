88b021d46efff2d9aff78cdd46e4944bf8c8dd2d
GARVIT JAIN <garvit@wednesday.is>
2023-11-23 18:28:58 +0530

    Add internationalized text for all iTunes UI

--- app/components/TrackCard/index.js
+++ app/components/TrackCard/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import styled from '@emotion/styled';
+import T from '@components/T/index';
 
 const CustomCard = styled(Card)`
   padding: 0.5rem;
@@ -34,7 +35,7 @@ const AudioPlayer = ({ previewUrl }) => {
   return (
     <>
       <Button variant="secondary" sx={{ border: '1px solid black', margin: '1rem 0' }} onClick={handleOnClick}>
-        {isPlaying ? 'Pause' : 'Preview'}
+        {isPlaying ? <T id="pause" /> : <T id="preview" />}
       </Button>
       <audio ref={audioRef} src={previewUrl} onEnded={() => setIsPlaying(false)} />
     </>
@@ -45,8 +46,12 @@ export default function TrackCard({ trackDetails }) {
   return (
     <CustomCard>
       <Image src={trackDetails.artworkUrl100} alt={`${trackDetails.trackName} thumbnail`} height={350} width={350} />
-      <h4>{trackDetails.trackName}</h4>
-      <p>Artist: {trackDetails.artistName}</p>
+      <T
+        id="track_name"
+        values={{ trackName: trackDetails.trackName }}
+        style={{ fontWeight: 'bold', margin: '1rem 0' }}
+      />
+      <T id="track_artist" values={{ artistName: trackDetails.artistName }} />
 
       <AudioPlayer previewUrl={trackDetails.previewUrl} />
     </CustomCard>
